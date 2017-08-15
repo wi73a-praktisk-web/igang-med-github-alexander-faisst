fee45c44b942f2f1980816f016cdf33e9597ec75
AlexanderFaisst <4201028@rts-365.dk>
2017-08-15 11:35:10 +0200

    Variabler & Arrays

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+/*
 console.log('Hej verden!');
 console.log('lalala');
 console.log('lalalalalalalalalaalalalallllll');
 console.log('another line');
+*/
+
+s = "app.js";
+console.log(s);
+
+pris = 5;
+antal = 20;
+console.log("x = " + pris);
+console.log("y = " + antal);
+console.log("x + y = " + (pris + antal));
+console.log("x - y = " + (pris - antal));
+console.log("y - x = " + (antal - pris));
+
+var s = "prisen er " + pris + " og antallet er " + antal + " og i alt bliver det så " + (pris * antal);
+console.log(s);
+
+var stringystring = "variabler";
+
+var s2 = `man kan indsætte ${stringystring} i sin tekst`;
+console.log(s2);
---
+++ øvelser.js
@@ -0,0 +1,262 @@
+//Øvelse 1.1 
+var navn = "Alex";
+
+//Øvelse 1.2 
+
+var alder = "24";
+
+//Øvelse 1.3 
+
+var tekst = `Mit navn er ${navn} og jeg er ${alder} år gammel`;
+
+// Øvelse 1.4 
+
+console.log("tekst = " + tekst);
+
+//Øvelse 2.1 
+
+var moms = 1.25;
+
+//Øvelse 2.2 
+
+var pris_uden_moms = 400;
+
+//Øvelse 2.3 
+
+var pris_med_moms = pris_uden_moms * moms;
+
+//Øvelse 2.4 
+
+console.log("pris_med_moms = " + pris_med_moms);
+
+//Øvelse 2.5 
+
+var pris_uden_moms_02 = pris_med_moms/moms;
+
+//Øvelse 2.6
+
+console.log("pris_uden_moms_02 = " + pris_uden_moms_02);
+
+//Øvelse 3.1
+
+var forste = 10;
+console.log("forste = " + forste);
+//Øvelse 3.2
+
+var anden = 20;
+console.log("anden = " + anden);
+
+//Øvelse 3.3
+
+var tredje = forste;
+forste = anden;
+anden = tredje;
+
+//Øvelse 3.4
+
+console.log("forste = " + forste);
+console.log("anden = " + anden);
+
+//Øvelse 4.1
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var carName = "Volvo";
+    document.getElementById("demo").innerHTML = carName;
+    </script>
+
+    </body>
+    </html>
+*/
+//Øvelse 4.2 
+
+/* 
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var number = 50;
+    document.getElementById("demo").innerHTML = number;
+    </script>
+
+    </body>
+    </html>
+*/ 
+//Øvelse 4.3
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var carName = "Volvo";
+    document.getElementById("demo").innerHTML = carName;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 4.4
+
+/* 
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var x = 5;
+    var y = 10;
+    document.getElementById("demo").innerHTML = x + y;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 4.5
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var x = 5;
+    var y = 10;
+    var z = x + y;
+    document.getElementById("demo").innerHTML = z;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 4.6
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo">Display the result here.</p> 
+
+    <script>
+    var firstName = "John", lastName = "Doe", age = 35;
+
+    document.getElementById("demo").innerHTML = 
+    firstName + " " + lastName + " is " + age;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 5.1
+
+/* 
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo"></p> 
+
+    <script>
+    var cars = ["Saab", "Volvo", "BMW"];
+    document.getElementById("demo").innerHTML = cars;
+    </script>
+
+    </body>
+    </html>
+*/
+
+
+//Øvelse 5.2
+
+/* 
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo"></p> 
+
+    <script>
+    var cars = ["Saab", "Volvo", "BMW"];
+    document.getElementById("demo").innerHTML = cars[1];
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 5.3
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo"></p> 
+
+    <script>
+    var cars = ["Opel", "Volvo", "BMW"];
+    document.getElementById("demo").innerHTML = cars;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 5.4
+
+/* 
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo"></p> 
+
+    <script>
+    var cars = ["Saab", "Volvo", "BMW"];
+    document.getElementById("demo").innerHTML = cars.length;
+    </script>
+
+    </body>
+    </html>
+*/
+
+//Øvelse 5.5
+
+/*
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <p id="demo"></p> 
+
+    <script>
+    var cars = ["Saab", "Volvo", "BMW"];
+    cars[cars.length] = "Mercedes";
+    document.getElementById("demo").innerHTML = cars;
+    </script>
+    
+    </body>
+    </html>
+*/
+
