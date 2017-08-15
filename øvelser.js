347525e3ded1d5e3e73dd7c4d434d616c0fe72aa
AlexanderFaisst <4201028@rts-365.dk>
2017-08-15 11:44:56 +0200

    Småting

--- øvelser.js
+++ øvelser.js
@@ -259,6 +259,8 @@ console.log("anden = " + anden);
     </body>
     </html>
 */
+
+//Øvelse 6.1,6.2 & 6.3
 var navne = ["Alex Faisst", "Peter Hansen", "Malte Mulbjerg", "Jesper Rasmussen", "Sofie Nielsen", "Rita Andersen"];
 var fodselsdage = ["03,05,1991","17,11,1995","21,07,1776","16,05,1994","22,06,1996","27,09,1998"];
 var bynavne = ["København","Odense","Aarhus","Frederiksværk","Roskilde", "Holbæk"];
