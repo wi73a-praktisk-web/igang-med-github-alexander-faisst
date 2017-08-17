3becc1239d9ccd78b77c18c3ffb5fe68ba07a632
AlexanderFaisst <4201028@rts-365.dk>
2017-08-17 08:40:09 +0200

    cpr_checker now works perfectly, precisely and beautifully

--- cpr_number_check.js
+++ cpr_number_check.js
@@ -1,5 +1,25 @@
-var cpr_number = "0104909995";
+/*
+    this version right now is super basic
+    it checks for: 
+        * the number being 10 characters long
+        * those characters being nothing but digits
+        * the day, month and year having correct values
+        * the 7th digit (technically) matching the year's value
+    if either one of the cases above fail, the program breaks or throws a simple error message
+    
+    NOTE: som algoritmen er skrevet lige nu, kan den godt spytte mærkelige resultater ud ift. sammenligningen af det 7. ciffer og årstallet - 
+    dette skyldes at det 7. ciffer både kan indikere at man er født helt tilbage i 1858, eller så langt inde i fremtiden som 2057. 
 
+    Jeg vil undersøge om man kan programmere sig uden om dette problem, men hvis ikke, så kan det være at der bare kræves en vis mængde 
+    sund fornuft udover de funktioner som algoritmen dækker over. 
+
+    Derudover har jeg tænkt mig at tilføje funktionalitet der siger noget om, om man er mand eller kvinde baseret på sit cpr nummer - 
+    teknisk set findes dette ikke længere siden 2007, men sådan bare for sjovt - for my purposes it should still largely work.  
+*/
+
+/* var cpr_number = "0104909995"; */
+/* var cpr_number = "0411923193"; */
+var cpr_number = "1509819996";
 
 function checkCPR(number) {
     //check at nummeret har den rigtige længde
@@ -23,6 +43,7 @@ function checkCPR(number) {
         console.log("year = " + year);
         var cp7 = parseInt(cpr_number[6]);
         console.log("cp7 = " + cp7);
+        var gender = getGender(cpr_number.charAt(9));
         //check at dagen har en korrekt værdi
         if (day >= 1 && day <= 31) {
             console.log("the day is valid")
@@ -35,44 +56,59 @@ function checkCPR(number) {
                     //check at det 7'ende ciffer har en korrekt værdi
                     switch (cp7) {
                         //cp7 between 0-3 (both inclusive)
-                        case 0: case 1: case 2: case 3: 
+                        case 0: case 1: case 2: case 3:
                             if (year >= 00 && year <= 99) {
-                                console.log("cipher #7 checks out");
-                            }
-                            else {
-                                console.log("the seventh cipher and the year don't match, cpr number is invalid")
+                                var fullYear = parseInt("19" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
+                                /* console.log("cipher #7 checks out"); */
                             }
                             break;
                         //cp7 is 4
                         case 4:
-                            if ((year >= 00 && year <= 36) || (year >= 37 && year <= 99)) {
-                                console.log("cipher #7 checks out");
+                            if (year >= 00 && year <= 36) {
+                                var fullYear = parseInt("20" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
                             }
-                            else {
-                                console.log("the seventh cipher and the year don't match, cpr number is invalid")
+                            else if (year >= 37 && year <= 99) {
+                                var fullYear = parseInt("19" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
+                                /* console.log("cipher #7 checks out"); */
                             }
                             break;
                         //cp7 between 5 and 8 (both inclusive)
                         case 5: case 6: case 7: case 8:
-                            if ((year >= 00 && year <= 57) || (year >= 58 && year <= 99)) {
-                                console.log("cipher #7 checks out");
+                            if (year >= 00 && year <= 57) {
+                                var fullYear = parseInt("20" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
                             }
-                            else {
-                                console.log("the seventh cipher and the year don't match, cpr number is invalid")
+                            else if (year >= 58 && year <= 99) {
+                                var fullYear = parseInt("18" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
+                                /* console.log("cipher #7 checks out"); */
                             }
                             break;
                         //cp7 is 9
                         case 9:
-                            if ((year >= 00 && year <= 36) || (year >= 37 && year <= 99)) {
-                                console.log("cipher #7 checks out");
+                            if (year >= 00 && year <= 36) {
+                                var fullYear = parseInt("20" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
                             }
-                            else {
-                                console.log("the seventh cipher and the year don't match, cpr number is invalid")
+                            else if (year >= 37 && year <= 99) {
+                                var fullYear = parseInt("19" + year);
+                                var age = 2017 - fullYear;
+                                console.log("the person's age is = " + age + " and the gender is = " + gender);
+                                /* console.log("cipher #7 checks out"); */
                             }
                             break;
                         //cp7 is invalid
                         default:
-                        console.log("ERROR ERROR ERROR ERROR")
+                            console.log("ERROR ERROR ERROR ERROR")
                             break;
                     }
                 }
@@ -90,6 +126,16 @@ function checkCPR(number) {
     }
     else {
         console.log("the number has the wrong length and is invalid ")
+    }
+}
+
+function getGender(n) {
+    var x = n % 2;
+    if (x == 0) {
+        return "woman";
+    }
+    else if (x == 1) {
+        return "man";
     }
 }
 
