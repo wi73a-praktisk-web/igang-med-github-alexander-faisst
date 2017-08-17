971331857eee83e4db0b34a55f0f03ee81ddc069
AlexanderFaisst <4201028@rts-365.dk>
2017-08-17 10:50:59 +0200

    functions #2 with anonymous

--- øvelser_functions.js
+++ øvelser_functions.js
@@ -1,3 +1,28 @@
+/* function add(n){
+    result += n;
+    console.log("result = " + result);
+}
+function subtract(n){
+    result -= n;
+    console.log("result = " + result);
+}
+function divide(n){
+    result /= n;
+    console.log("result = " + result);
+}
+function multiply(n){
+    result *= n;
+    console.log("result = " + result);
+}
+function pi(){
+    //what am i even gonna do here? (Math.PI, but what am i gonna use it for in this instance?)
+}
+function sqrRt(){
+    result = Math.sqrt(result);
+    console.log("result = " + result);
+} */
+
+
 //Øvelse 1.A
 
 var firstName = "Alex";
@@ -12,29 +37,30 @@ print(firstName,surName,birthDay);
 
 //Øvelse 1.B
 var x = 10;
-var y = 20;
-var result;
+var result = 0;
 
-function add(n,m){
-    result = n + m;
-    console.log("result = " + result);
-}
-function subtract(n,m){
-    result = n - m;
-    console.log("result = " + result);
-}
-function divide(n,m){
-    result = n /m;
-    console.log("result = " + result);
-}
-function multiply(n,m){
-    result = n * m;
-    console.log("result = " + result);
-}
-function pi(){
-    //what am i even gonna do here? (Math.PI, but what am i gonna use it for in this instance?)
-}
-function sqrRt(m){
-    result = Math.sqrt(m);
-    console.log("result = " + result);
-}
+var add = n => console.log("result = " + (result += n));
+var subtract = n => console.log("result = " + (result -= n));
+var divide = n => console.log("result = " + (result /= n));
+var multiply = n => console.log("result = " + (result *= n));
+var sqRt = n => console.log("result = " + Math.sqrt(result));
+
+add(x);
+subtract(x);
+divide(x);
+multiply(x);
+sqRt();
+
+//Øvelse 2
+var output;
+var s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum libero nec ante tristique";
+var n = 0;
+var m = 10;
+
+var substring = (s,n,m) => console.log(s.substring(n,m));
+var split = s => console.log(s.split(" ")); //... could also split at commas or anything else
+var slice = (s,n,m) => console.log(s.split(n,m));
+
+substring(s,0,10);
+split(s);
+slice(s,n,m);
