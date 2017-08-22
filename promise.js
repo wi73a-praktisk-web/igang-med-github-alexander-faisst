9bad77766ddef2423a2fee6e3ef147c727a59b24
AlexanderFaisst <4201028@rts-365.dk>
2017-08-22 19:28:37 +0200

    experimented a bit with promises, now have a slightly more profitient understanding of the matter

---
+++ promise.js
@@ -0,0 +1,25 @@
+function thirdPromise(someBoolValue) {
+    return new Promise((resolve, reject) => {
+    //    setTimeout(() => {
+          if (someBoolValue) {
+             resolve('Denne besked kommer fra "resolve"');
+          } else {
+             reject(new Error('Det var ikke en succes, dette kommer fra "reject"'));
+          }
+    //    }, 2000);
+    })
+ }
+ 
+ thirdPromise(true)
+    .then((success) => {
+       console.log(success);
+       //throw new Error('lad os sige noget gik galt!');
+       return thirdPromise(true);
+    }).then((nextSuccess) => {
+       console.log(nextSuccess);
+       return thirdPromise(false);
+    }).then((nextSuccess) => {
+        console.log(nextSuccess)
+    }).catch((error) => {
+       console.log(error);
+    })
