10d1f8feea700528c989d6d821b9aa20e9b2ab6c
AlexanderFaisst <4201028@rts-365.dk>
2017-08-23 23:17:27 +0200

    turned every single if statement into a ternary expression, very neat and fewer lines of code

--- cpr_checker_promises.js
+++ cpr_checker_promises.js
@@ -8,60 +8,31 @@ var day = cpr_number.charAt(0) + cpr_number.charAt(1),
 console.log("day = " + day + "month = " + month + "year = " + year + "cp7 = " + cp7);
 
 //check at dagen har en korrekt værdi
-var checkDay = number => {
-    return new Promise((resolve, reject) => {
-        if (number >= 1 && number <= 31) {
-            resolve('the day is valid');
-        } else {
-            reject(new Error('the day is invalid'));
-        }
+var checkDay = number => {return new Promise((resolve, reject) => {number >= 1 && number <= 31 ? resolve('the day is valid') : reject(new Error('the day is invalid'));
     });
 }
 
 //check at måneden har en korrekt værdi
 var checkMonth = number => {
-    return new Promise((resolve, reject) => {
-        if (number >= 1 && number <= 12) {
-            resolve('the month is valid');
-        } else {
-            reject(new Error('the month is invalid'));
-        }
-    })
+    return new Promise((resolve, reject) => {number >= 1 && number <= 12 ? resolve('the month is valid') : reject(new Error('the month is invalid'));
+    });
 }
 
 //check at året har en korrekt værdi
 var checkYear = number => {
-    return new Promise((resolve, reject) => {
-        if (number >= 0 && number <= 99) {
-            resolve('the year is valid');
-        } else {
-            reject(new Error('the year is invalid'));
-        }
-    })
+    return new Promise((resolve, reject) => {number >= 0 && number <= 99 ? resolve('the year is valid') : reject(new Error('the year is invalid'));
+    });
 }
 
 //check at nummeret har den rigtige længde
 let checkLength = s => {
-    return new Promise((resolve, reject) => {
-        if (s.length === 10) {
-            resolve('the cpr number has the correct length');
-        } else {
-            reject(new Error('cpr number has an invalid length'));
-        }
+    return new Promise((resolve, reject) => { s.length === 10 ? resolve('the cpr number has the correct length') : reject(new Error('cpr number has an invalid length'));
     })
 }
 
 //check at nummeret består af tal
 var checkIfNan = number => {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < cpr_number.length - 1; i++) {
-            //check for hver character i cpr nummeret om det er et tal
-            if (!isNaN(parseInt(cpr_number[i]))) {
-                resolve("success, all character are digits");
-            }
-            else {
-                reject("cpr number contains a letter and is hence invalid");
-            }
+    return new Promise((resolve, reject) => {for (let i = 0; i < cpr_number.length - 1; i++) { !isNaN(parseInt(cpr_number[i])) ? resolve("success, all character are digits") : reject("cpr number contains a letter and is hence invalid");
         }
     })
 }
@@ -73,54 +44,19 @@ var checkCP7 = number => {
         switch (cp7) {
             //cp7 between 0-3 (both inclusive)
             case 0: case 1: case 2: case 3:
-                if (year >= 00 && year <= 99) {
-                    var fullYear = parseInt("19" + year);
-                    var age = 2017 - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                break;
+                year >= 00 && year <= 99 ?  resolve("the person's age is = " + (2017 - parseInt("19" + year)) + " and the gender is = " + gender) : reject();
             //cp7 is 4
             case 4:
-                if (year >= 00 && year <= 36) {
-                    var fullYear = parseInt("20" + year);
-                    var age = new Date().getFullYear() - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                else if (year >= 37 && year <= 99) {
-                    var fullYear = parseInt("19" + year);
-                    var age = new Date().getFullYear() - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                break;
+                year >= 00 && year <= 36 ? resolve("the person's age is = " + new Date().getFullYear() -  parseInt("20" + year) + " and the gender is = " + gender) : year >= 37 && year <= 99 ? resolve("the person's age is = " + new Date().getFullYear() - parseInt("19" + year) + " and the gender is = " + gender) : reject();
             //cp7 between 5 and 8 (both inclusive)
             case 5: case 6: case 7: case 8:
-                if (year >= 00 && year <= 57) {
-                    var fullYear = parseInt("20" + year);
-                    var age = new Date().getFullYear() - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                else if (year >= 58 && year <= 99) {
-                    var fullYear = parseInt("18" + year);
-                    var age = new Date().getFullYear() - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                break;
+                year >= 00 && year <= 57 ? resolve("the person's age is = " + new Date().getFullYear() - parseInt("20" + year) + " and the gender is = " + gender) : year >= 58 && year <= 99 ? resolve("the person's age is = " + new Date().getFullYear() - parseInt("18" + year) + " and the gender is = " + gender) : reject(); 
             //cp7 is 9
             case 9:
-                if (year >= 00 && year <= 36) {
-                    var fullYear = parseInt("20" + year);
-                    var age = 2017 - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                else if (year >= 37 && year <= 99) {
-                    var fullYear = parseInt("19" + year);
-                    var age = 2017 - fullYear;
-                    resolve("the person's age is = " + age + " and the gender is = " + gender);
-                }
-                break;
+                year >= 00 && year <= 36 ? resolve("the person's age is = " + 2017 - parseInt("20" + year) + " and the gender is = " + gender) : year >= 37 && year <= 99 ? resolve("the person's age is = " + 2017 - parseInt("19" + year) + " and the gender is = " + gender) : reject();
             //cp7 is invalid
             default:
-                reject("the 7th digit is invalid")
+                console.log("cp7 is invalid");
                 break;
         }
     })
